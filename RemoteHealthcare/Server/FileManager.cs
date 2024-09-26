96fdbc0796db727dfea4d262167a0464073fcc0e
Jahper <jaspertr04@gmail.com>
2024-09-26 15:52:45 +0200

    comments verder aangepast

--- RemoteHealthcare/Server/FileManager.cs
+++ RemoteHealthcare/Server/FileManager.cs
@@ -3,10 +3,11 @@ namespace Server;
 public class FileManager
 {
     //TODO: bestand voor beheren geregistreerde clients
+    //server stuurt data hiernaar en vergelijkt hashes om te kijken of het geldig is
     
     //TODO: bestand voor beheren geregistreerde artsen
+    //server stuurt data hiernaar en vergelijkt hashes om te kijken of het geldig is
 
     //TODO: bestand per geregistreerde client om sessiewaardes op te slaan
-    
-    
+    //per client een mapje maken met alle historische data
 }
