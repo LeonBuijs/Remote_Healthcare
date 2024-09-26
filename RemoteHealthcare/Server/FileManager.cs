f86992d3e71d31550c05faa6fbc53c7bb55b5a61
Jahper <jaspertr04@gmail.com>
2024-09-26 15:29:27 +0200

    FileManager toegevoegd en TODO's erin gezet

---
+++ RemoteHealthcare/Server/FileManager.cs
@@ -0,0 +1,12 @@
+namespace Server;
+
+public class FileManager
+{
+    //TODO: bestand voor beheren geregistreerde clients
+    
+    //TODO: bestand voor beheren geregistreerde artsen
+
+    //TODO: bestand per geregistreerde client om sessiewaardes op te slaan
+    
+    
+}
