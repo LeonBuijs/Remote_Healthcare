1c151adf252f1339adafdc1d2d40b97a38a6e1a3
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-09-30 19:07:04 +0200

    Error afhandeling verbeterd en al een paar commando's voor de VR Engine

--- RemoteHealthcare/Client/VRConnection/Program.cs
+++ RemoteHealthcare/Client/VRConnection/Program.cs
@@ -11,12 +11,66 @@ class VRConnection
         // Stap 1
         TcpClient tcpClient = new TcpClient();
         tcpClient.Connect("85.145.62.130", 6666);
+        tcpClient.ReceiveTimeout = 10000;
         NetworkStream stream = tcpClient.GetStream();
         Console.WriteLine("Verbonden met de server\n");
 
         CreateTunnel(stream);
+        // SetTime(stream, 0);
         
-        SetTime(stream, 0);
+        
+        SendPacket(stream, "{\"id\" :" +
+                "\"tunnel/send\", " +
+                "\"data\" :" +
+                "{\"dest\" :\"" +
+                SessionID +
+                "\", " +
+                "\"data\" :" +
+                "{\"id\" : \"scene/node/add\", " +
+                "\"data\" :" +
+                "{\"name\" : \"test\", " +
+                "\"components\" :" +
+                // "{\"transform\" : " +
+                // "{\"position\" : [ 0, 0, 0 ], " +
+                // "\"scale\" : 1, " +
+                // "\"rotation\" : [ 0, 0, 0 ]}," +
+                // "\"model\" : " +
+                // "{\"file\" : \"filename\", " +
+                // "\"cullbackfaces\" : true, " +
+                // "\"animated\" : false, " +
+                // "\"animation\" : \"animationname\"}," +
+                // "\"terrain\" : " +
+                // "{\"smoothnormals\" : true}, " +
+                // "\"panel\" : " +
+                // "{\"size\" : [ 1, 1 ], " +
+                // "\"resolution\" : [ 512, 512 ], " +
+                // "\"background\" : [ 1, 1, 1, 1], " +
+                // "\"castShadow\" : true}, " +
+                // "\"water\" :" +
+                // "{\"size\" : [ 20, 20 ], " +
+                // "\"resolution\" : 0.1}
+                 "}}}}");
+        
+        
+        
+        RecievePacket(stream);
+        RecievePacket(stream);
+        
+        // SendPacket(stream, "{\"id\" : " +
+        //                    "\"tunnel/send\", " +
+        //                    "\"data\" :" +
+        //                    "{\"dest\" : \"" +
+        //                    SessionID +
+        //                    "\", " +
+        //                    "\"data\" :" +
+        //                    "{\"id\" : \"scene/panel/drawtext\", " +
+        //                    "\"data\" : " +
+        //                    "{\"id\" : {1}, " +
+        //                    "\"text\" : \"Hello World\", " +
+        //                    "\"position\" : [ 100.0, 100.0 ], " +
+        //                    "\"size\" : 128.0, " +
+        //                    "\"color\" : [ 0,0,0,1 ], }}}}");
+        // RecievePacket(stream);
     }
 
     private static void SetTime(NetworkStream stream, int time)
@@ -79,19 +133,45 @@ class VRConnection
         Array.Copy(prefix, 0, combinedArray, 0, prefix.Length);
         Array.Copy(array, 0, combinedArray, prefix.Length, array.Length);
         stream.Write(combinedArray, 0, combinedArray.Length);
-        Console.WriteLine(BitConverter.ToString(combinedArray));
+        Console.WriteLine(Encoding.ASCII.GetString(combinedArray, 0, combinedArray.Length));
         Console.WriteLine("Bericht verstuurd\n");
     }
 
     private static string RecievePacket(NetworkStream stream)
     {
-        var buffer = new byte[2048];
-        var bytesRead = stream.Read(buffer, 0, buffer.Length);
+        try
+        {
+            var buffer = new byte[2048];
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
 
-        int bufferLength = buffer[0];
-        Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
-        
-        Console.WriteLine("Bericht ontvangen\n");
-        return Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            int bufferLength = buffer[0];
+
+            if (Encoding.ASCII.GetString(buffer, 0, bytesRead).StartsWith("{"))
+            {
+                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+                Console.WriteLine("Bericht ontvangen\n");
+                return Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+            }
+
+            if (Encoding.ASCII.GetString(buffer, 0, bytesRead).Length < 1)
+            {
+                Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+                Console.WriteLine("Bericht ontvangen\n");
+                return Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            }
+
+            string returnString = Encoding.ASCII.GetString(buffer, 0, bytesRead).Substring(1);
+
+            Console.WriteLine(returnString);
+            Console.WriteLine("Bericht ontvangen\n");
+
+            return returnString;
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("Geen reactie...");
+            return "";
+        }
     }
 }
