a6c190d89fedcd7ec28a7a61442a166b4d792d41
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-10-17 15:50:35 +0200

    Camera gekoppeld aan de fiets

--- RemoteHealthcare/Client/VRConnection/Program.cs
+++ RemoteHealthcare/Client/VRConnection/Program.cs
@@ -22,13 +22,15 @@ class VRConnection
         // SetTime(stream, 0);
         string uuidBike = CreateNodeForBike(stream);
         // string uuidTerrain = CreateNodeForTerrain(stream);
-
+        
         string uuidRoute = CreateRoute(stream);
         CreateRoad(stream, uuidRoute);
         
         FollowRoute(stream, uuidRoute, uuidBike);
         
         ChangeFollowRouteSpeed(stream, uuidBike, 10);
+        
+        AttachCameraToBike(stream, uuidBike);
 
 
         // SendPacket(stream, "{\"id\" : " +
@@ -46,6 +48,90 @@ class VRConnection
         //                    "}}}}");
         // RecievePacket(stream);
     }
+
+    private static void AttachCameraToBike(NetworkStream stream, string uuidBike)
+    {
+        SendPacket(stream, "{\"id\" : " +
+                           "\"tunnel/send\", " +
+                           "\"data\" :" +
+                           "{\"dest\" : \"" +
+                           SessionID +
+                           "\", " +
+                           "\"data\" :" +
+                           "{\"id\" : \"scene/get\", " +
+                           "}}}");
+        JsonObject jsonObject = (JsonObject)JsonObject.Parse(RecievePacket(stream));
+        JsonArray dataArray = (JsonArray)jsonObject["data"]["data"]["data"]["children"];
+        string cameraUuid = null;
+        foreach (JsonObject child in dataArray)
+        {
+            string name = child["name"].ToString();
+            if (name == "Camera")
+            {
+                cameraUuid = child["uuid"].ToString();
+            }
+        }
+
+        SendPacket(stream, "{\"id\" : " +
+                           "\"tunnel/send\", " +
+                           "\"data\" :" +
+                           "{\"dest\" : \"" +
+                           SessionID +
+                           "\", " +
+                           "\"data\" :" +
+                           "{\"id\" : \"scene/node/update\", " +
+                           "\"data\" : " +
+                           "{\"id\" : \"" + cameraUuid + "\", " +
+                           "\"parent\" : \"" + uuidBike + "\"," +
+                           "\"transform\" :" +
+                           "{\"position\" : [ 1, 0, 2 ]," +
+                           "\"scale\" : 1.0," +
+                           "\"rotation\" : [ 0, 180, 0 ]," +
+                           "}}}}}");
+        RecievePacket(stream);
+    }
+    // private static void GenerateTerrain(int width, int height, int[] terrainMap)
+    // {
+    //     float scale = 20f;  // Bepaalt hoe "heuvelachtig" het terrein is
+    //     int maxHeight = 20; // Maximale hoogte van het terrein
+    //
+    //     for (int y = 0; y < height; y++)
+    //     {
+    //         for (int x = 0; x < width; x++)
+    //         {
+    //             float sampleX = x / scale;
+    //             float sampleY = y / scale;
+    //             float noiseValue = PerlinNoise(sampleX, sampleY);
+    //             
+    //             // Schaal de noise waarde naar een geheel getal voor het terrein
+    //             terrainMap[y * width + x] = (int)((noiseValue + 1) * (maxHeight / 2)); // Zorgt ervoor dat de minimale waarde 0 is
+    //         }
+    //     }
+    // }
+    //
+    // // Simpele noise functie
+    // private static float PerlinNoise(float x, float y)
+    // {
+    //     return (float)(Math.Sin(x) + Math.Sin(y)) / 2;  // De waarden liggen tussen -1 en 1
+    // }
+    //
+    // private static string ConvertArrayToJson(int[] array)
+    // {
+    //     StringBuilder jsonBuilder = new StringBuilder();
+    //     jsonBuilder.Append("[");
+    //     
+    //     for (int i = 0; i < array.Length; i++)
+    //     {
+    //         jsonBuilder.Append(array[i]);
+    //         if (i < array.Length - 1)
+    //         {
+    //             jsonBuilder.Append(",");
+    //         }
+    //     }
+    //     
+    //     jsonBuilder.Append("]");
+    //     return jsonBuilder.ToString();
+    // }
 
     private static void ChangeFollowRouteSpeed(NetworkStream stream, string uuidBike, int speed)
     {
@@ -130,29 +216,35 @@ class VRConnection
 
     private static void createTerrain(NetworkStream stream)
     {
+        // Heuvellandschap
+        
+        // int[] terrainMap = new int[256 * 256];
+        // GenerateTerrain(256, 256, terrainMap);
+        // string jsonString = ConvertArrayToJson(terrainMap);
+        
         int[] newArray = new int[256 * 256];
-
+        
         for (int i = 0; i < newArray.Length; i++)
         {
             newArray[i] = 1;
         }
-
+        
         StringBuilder jsonBuilder = new StringBuilder();
         jsonBuilder.Append("[");
-
+        
         for (int i = 0; i < newArray.Length; i++)
         {
             jsonBuilder.Append(newArray[i]);
-
+        
             // Voeg een komma toe als dit niet het laatste element is
             if (i < newArray.Length - 1)
             {
                 jsonBuilder.Append(",");
             }
         }
-
+        
         jsonBuilder.Append("]");
-
+        
         // De resulterende JSON-string
         string jsonString = jsonBuilder.ToString();
 
@@ -165,7 +257,7 @@ class VRConnection
                            "\"data\" :" +
                            "{\"id\" : \"scene/terrain/add\", " +
                            "\"data\" : " +
-                           "{\"size\" : [ 3, 3 ], " +
+                           "{\"size\" : [ 256, 256 ], " +
                            "\"heights\" : " + jsonString +
                            "}}}}");
         RecievePacket(stream);
