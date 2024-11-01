66597feb86e3b19af0d69b16d8cca2e943f816e0
LeonBuijs <77409149+LeonBuijs@users.noreply.github.com>
2024-11-01 12:19:29 +0100

    Test toegevoegd  voor start session

--- RemoteHealthcare/UnitTests/UnitTest1.cs
+++ RemoteHealthcare/UnitTests/UnitTest1.cs
@@ -6,6 +6,10 @@ namespace ServerTests
 {
     public class ServerTests
     {
+        /**
+        * Test controleert of de toegang van de dokter correct wordt ingesteld
+        * op basis van de opgegeven inloggegevens.
+        */
         [Test]
         [TestCase("test", "test", true)]
         [TestCase("testFout", "test", false)]
@@ -36,6 +40,48 @@ namespace ServerTests
 
             // Assert
             Assert.AreEqual(expectedAccess, mockConnection.Object.Access);
+        }
+        
+        /**
+         * Deze test controleert of de client correct in een sessie wordt gezet
+         * wanneer de StartSession-methode wordt aangeroepen met de juiste client-ID.
+         */
+        [Test]
+        [TestCase("client1", true)]
+        [TestCase("client2", false)]
+        public void StartSession_ShouldSetClientInSession(string clientId, bool expectedInSession)
+        {
+            // Arrange
+            var mockConnection = new Mock<Connection>();
+            var mockServer = new Server.Server();
+            mockServer.clients.Add(clientId, new ClientConnection(clientId, mockConnection.Object));
+            
+            var messageParts = new string[] { "1", clientId }; 
+
+            // Act
+            mockServer.StartSession(messageParts);
+
+            // Assert
+            Assert.AreEqual(expectedInSession, mockServer.clients[clientId].InSession);
+        }
+
+        /**
+         * Deze test controleert of er niets gebeurt als er geprobeerd wordt
+         * een sessie te starten voor een client die niet bestaat.
+         */
+        [Test]
+        [TestCase("nonExistentClient")]
+        public void StartSession_ShouldNotSetClientInSession_WhenClientDoesNotExist(string clientId)
+        {
+            // Arrange
+            var mockServer = new Server.Server();
+            var messageParts = new string[] { "1", clientId };
+
+            // Act
+            mockServer.StartSession(messageParts);
+
+            // Assert
+            Assert.That(mockServer.clients.ContainsKey(clientId), Is.False);
         }
     }
 }
